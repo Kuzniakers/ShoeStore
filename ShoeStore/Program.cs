05bfce6d767914cee1ceae0f29971e8d5948cfe5
Konrad Kuźniak <115842045+Kuzniakers@users.noreply.github.com>
2023-06-27 09:52:29 +0200

    Update Program.cs

--- ShoeStore/Program.cs
+++ ShoeStore/Program.cs
@@ -11,34 +11,25 @@ namespace ShoeStore
         {
             
             var builder = WebApplication.CreateBuilder(args);
-
             
             builder.Services.AddRazorPages();
             builder.Services.AddDbContext<ShoeStoreContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
-
             var host = CreateHostBuilder(args).Build();
             host.Run();
-
-            
+         
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Error");
-               
+                app.UseExceptionHandler("/Error");     
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.MapRazorPages();
-
             app.Run();
         }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
